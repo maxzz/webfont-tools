c1408633d4428b1866e39a00ce5cf70123d09b0e
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-25 18:36:28 -0700

    WIP. add basic page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Header } from "./components/1-header";
+import { Main } from "./components/2-main";
+import { Footer } from "./components/3-footer";
+
 export function App() {
     return (<>
-        <div>
-            Now
+        <div className="min-h-screen bg-green-400 grid grid-rows-[auto,1fr,auto]">
+            <Header />
+            <Main />
+            <Footer />
         </div>
     </>);
 }
