eedf95e59ecdcdab98cc0e6cffd16b0dc38da20c
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-25 19:37:58 -0700

    WIP. add tailwind default palettes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Footer } from "./components/3-footer";
 
 export function App() {
     return (<>
-        <div className="min-h-screen bg-green-400 grid grid-rows-[auto,1fr,auto]">
+        <div className="min-h-screen bg-primary-400 grid grid-rows-[auto,1fr,auto]">
             <Header />
             <Main />
             <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: colors.zinc,
+                secondary: colors.green,
+            },
+        },
     },
     plugins: [],
 }
