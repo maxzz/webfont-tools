328b8cdb8a3b678ad314a23396b53345d840195b
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-26 18:41:28 -0700

    WIP. add control focus styles

--- src/components/2-main/index.tsx
+++ src/components/2-main/index.tsx
@@ -4,6 +4,7 @@ import { classNames } from '@/utils';
 import { FontData, fontData, getFont } from '@/storage';
 
 const inputClasses = 'px-2 py-1 w-full bg-primary-200 border-primary-400 border rounded';
+export const inputFocusClasses = "focus:ring-primary-600 dark:focus:ring-primary-400 focus:ring-offset-primary-200 dark:focus:ring-offset-primary-800 focus:ring-1 focus:ring-offset-1 focus:outline-none";
 
 function Input({ store, name, label, className, ...rest }: { store: FontData, name: keyof FontData; label: string; } & InputHTMLAttributes<HTMLInputElement>) {
     const snap = useSnapshot(store);
@@ -21,7 +22,7 @@ function InputArea<T extends {}>({ store, name, label, className, ...rest }: { s
         <div className="">
             <div className="">{label}</div>
             <textarea
-                className={classNames(inputClasses, className)}
+                className={classNames(inputClasses, inputFocusClasses, className)}
                 value={snap[name] as string}
                 onChange={(e) => { (store as any)[name] = e.target.value; }}
                 {...rest}
@@ -32,7 +33,7 @@ function InputArea<T extends {}>({ store, name, label, className, ...rest }: { s
 
 function Button({ children, className, ...rest }: { children: ReactNode; } & HTMLAttributes<HTMLButtonElement>) {
     return (
-        <button className={classNames('px-4 py-2 bg-primary-200 border-primary-400 border rounded', className)} {...rest}>
+        <button className={classNames('px-4 py-2 bg-primary-200 border-primary-400 border rounded', inputFocusClasses, className)} {...rest}>
             {children}
         </button>
     );
@@ -43,29 +44,23 @@ async function convert(fontText: string) {
         return;
     }
     try {
-        // const encoder = new TextEncoder();
-        // const buffer = encoder.encode(fontText);
-        // getFont(buffer.buffer);
-
-        // const encoder = new TextEncoder();
-        // const buffer = encoder.encode(fontText);
-
         const res = await getFont(fontText);
         fontData.xmlText = res;
     } catch (error) {
         console.log(error);
+        fontData.xmlText = '';
     }
 }
 
 function ConvertForm() {
     return (
         <div className="flex flex-col space-y-2">
-            <InputArea className="h-64 text-xs" store={fontData} name="fontText" label="Woff2 font data" />
+            <InputArea className="h-64 text-xs" store={fontData} name="fontText" label="Woff2 font data" spellCheck="false" />
 
-            <InputArea className="h-64 text-xs" store={fontData} name="xmlText" label="SVG font" />
+            <InputArea className="h-64 text-xs" store={fontData} name="xmlText" label="SVG font" spellCheck="false" />
 
             <Button
-            className="self-end"
+                className="self-end"
                 onClick={() => {
                     convert(fontData.fontText);
                 }}
