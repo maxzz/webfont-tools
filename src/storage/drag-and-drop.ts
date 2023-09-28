d73afc3b35ceef0c6b7f60cafbe700206a10c201
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-27 19:40:23 -0700

    WIP. add loadFileData from dropped files

--- src/storage/drag-and-drop.ts
+++ src/storage/drag-and-drop.ts
@@ -1,4 +1,9 @@
 import { atom } from "jotai";
+import { loadFileData } from "@/utils";
+
+//data:application/octet-stream;base64,
+//data:image/png;base64,
+//data:application/font-woff2;charset=utf-8;base64, //tested on: https://fontawesome.com/icons/shield?f=classic&s=thin
 
 export type DoDroppedFilesAtom = typeof doDroppedFilesAtom;
 export const doDroppedFilesAtom = atom(
@@ -7,9 +12,8 @@ export const doDroppedFilesAtom = atom(
         if (!files.length) { return; }
         
         try {
-            console.log('dropped', files);
-
-            // const blob = await loadFileData(files[0]);
+            const blob = await loadFileData(files[0], { asArrayBuffer: false });
+            console.log('blob', blob);
             // const img: HTMLImageElement = await createImageFromBlob(blob);
             // set(orgImgAtom, img);
         } catch (error) {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from "./classnames";
 export * from "./debounce";
 export * from "./numbers";
+export * from "./load-file-as-blob";
