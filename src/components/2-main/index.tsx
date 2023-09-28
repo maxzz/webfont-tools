29a030ecc83611de034995ed59bb3204d7e55670
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-28 15:18:53 -0700

    WIP. font-data split source and result data

--- src/components/2-main/index.tsx
+++ src/components/2-main/index.tsx
@@ -5,13 +5,13 @@ import { InputArea, Button } from './controls';
 function ConvertForm() {
     return (
         <div className="flex flex-col space-y-2">
-            <InputArea className="h-64 text-xs" store={fontData} name="fontText" label="WOFF2 font data" spellCheck="false" />
+            <InputArea className="h-64 text-xs" store={fontDataSource} name="text" label="WOFF2 font data" spellCheck="false" />
 
             <InputArea className="h-64 text-xs" store={fontData} name="xmlText" label="SVG font" spellCheck="false" />
 
             <ShowGlyphs />
 
-            <Button className="self-end" onClick={() => convertToSvg(fontData.fontText)}>
+            <Button className="self-end" onClick={() => convertToSvg(fontDataSource.text)}>
                 Convert
             </Button>
         </div>
--- src/store/font-data.ts
+++ src/store/font-data.ts
@@ -1,4 +1,16 @@
-import { proxy } from "valtio";
+import { proxy, subscribe } from "valtio";
+import { GlyphAttributes } from "./types";
+import * as tests from './test-fonts';
+
+export type FontDataSource = {
+    text: string;
+};
+
+export const fontDataSource = proxy<FontDataSource>({
+    text: tests.defaultFontDataBase642,
+});
+
+//
 
 export type FontData = {
     fontText: string;
@@ -6,11 +18,19 @@ export type FontData = {
     glyphs: GlyphAttributes[];
 };
 
-import * as tests from './test-fonts';
-import { GlyphAttributes } from "./types";
-
 export const fontData = proxy<FontData>({
     fontText: tests.defaultFontDataBase642,
     xmlText: '',
     glyphs: [],
 });
+
+//
+
+subscribe(fontDataSource, () => {
+    console.log('fontDataSource changed');
+    fontData.fontText = fontDataSource.text;
+});
+
+export function setFontText(text: string) {
+    const re = /^\s*data:(?<media_type>(?<mime_type>[a-z\-]+\/[a-z\-\+]+)(?<params>(;[a-z\-]+\=[a-z\-]+)*))?(?<encoding>;base64)?,(?<data>[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*)$/gi;
+}
