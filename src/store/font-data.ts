f69262b1ea63ab2e00597495e9b017fc62053a54
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-28 17:10:59 -0700

    WIP. font-data add data URI matching

--- src/store/font-data.ts
+++ src/store/font-data.ts
@@ -2,12 +2,21 @@ import { proxy, subscribe } from "valtio";
 import { GlyphAttributes } from "./types";
 import * as tests from './test-fonts';
 
+type MatchUriData = {
+    mime: string;   // 'application/octet-stream' | 'image/png' | 'application/font-woff2'
+    enc?: string;   // charset=utf-8
+    base: string;   // base64
+    data: string;
+};
+
 export type FontDataSource = {
     text: string;
+    dataUri?: MatchUriData | undefined | null;
 };
 
 export const fontDataSource = proxy<FontDataSource>({
-    text: tests.defaultFontDataBase642,
+    // text: tests.defaultFontDataStr,
+    text: 'data:application/font-woff2;charset=utf-8;base64,1',
 });
 
 //
@@ -19,7 +28,7 @@ export type FontData = {
 };
 
 export const fontData = proxy<FontData>({
-    fontText: tests.defaultFontDataBase642,
+    fontText: '',
     xmlText: '',
     glyphs: [],
 });
@@ -28,9 +37,24 @@ export const fontData = proxy<FontData>({
 
 subscribe(fontDataSource, () => {
     console.log('fontDataSource changed');
+    
     fontData.fontText = fontDataSource.text;
+
+    checkDataUri(fontDataSource.text);
 });
 
-export function setFontText(text: string) {
-    const re = /^\s*data:(?<media_type>(?<mime_type>[a-z\-]+\/[a-z\-\+]+)(?<params>(;[a-z\-]+\=[a-z\-]+)*))?(?<encoding>;base64)?,(?<data>[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*)$/gi;
+//data:application/octet-stream;base64,
+//data:image/png;base64,
+//data:application/font-woff2;charset=utf-8;base64, //tested on: https://fontawesome.com/icons/shield?f=classic&s=thin
+
+export function checkDataUri(text: string) {
+    //const re = /^\s*data:(?<media_type>(?<mime_type>[a-z\-]+\/[a-z\-\+]+)(?<params>(;[a-z\-]+\=[a-z\-]+)*))?(?<encoding>;base64)?,(?<data>[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*)$/gi;
+    //const re = /^\s*data:(?<media>(?<mime>[a-z\-]+\/[a-z\-\+]+)(?<params>(;[a-z\-]+\=[a-z\-]+)*))?(?<encoding>;base64)?,(?<data>.*\s*)$/gi;
+    const re = /^data:(?<mime>[\w\/\+-]*);?(?<enc>(?:charset=[\w-]+)?);?(?<base>base64?),(?<data>[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%]*)\s*/gi;
+    //OK const re = /data:(?<mime>[\w/\-\.]+);(?<encoding>\w+),(?<data>.*)/gi;
+    //const re = /data:(?<data>[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*)$/gi;
+    const match = re.exec(text);
+    fontDataSource.dataUri = match?.groups as MatchUriData;
+
+    console.log('match', match);
 }
