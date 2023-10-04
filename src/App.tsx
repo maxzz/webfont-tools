08cf49804063af46ce3ed9198cde88f42a9e2556
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-03 20:48:11 -0700

    WIP. shadcn. add theme-provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { Header } from "./components/1-header";
 import { Main } from "./components/2-main";
 import { Footer } from "./components/3-footer";
 import { DropArea, UIToaster } from "./components/ui";
+import { ThemeProvider } from "./components/ui/shadcn/theme-provider";
 
 export function App() {
     return (<>
-        <UIToaster />
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <UIToaster />
 
-        <div className="min-h-screen grid grid-rows-[auto,1fr,auto]">
-            <Header />
-            <Main />
-            <Footer />
-        </div>
-        
-        <DropArea />
+            <div className="min-h-screen grid grid-rows-[auto,1fr,auto]">
+                <Header />
+                <Main />
+                <Footer />
+            </div>
+
+            <DropArea />
+        </ThemeProvider>
     </>);
 }
