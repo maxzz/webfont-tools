3acce63d6ab1a547d3a8411d4b32da1cd807bbd4
Max Zakharzhevskiy <maxzz@msn.com>
2023-10-05 20:10:16 -0700

    WIP. add split build to chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+function manualChunks(id: string) { //https://rollupjs.org/configuration-options/#output-manualchunks
+    if (id.includes("@radix-ui")) {
+        return "radix-ui";
+    }
+    if (id.includes("fonteditor-core")) {
+        return "fonteditor";
+    }
+    if (id.includes("node_modules")) {
+        return "vendor";
+    }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +26,24 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
-    plugins: [react()],
+    build: {
+        // chunkSizeWarningLimit: 600,
+        target: "esnext",
+        minify: 'esbuild',
+        rollupOptions: {
+            output: {
+                manualChunks,
+            },
+        },
+    },
+    plugins: [
+        react(),
+
+        visualizer({
+            filename: 'visualization.html',
+            template: 'sunburst', // sunburst - d3 style (good as default as well); treemap - table (default); network - graph (slow to open).
+            gzipSize: true,
+            brotliSize: true,
+        }),
+    ],
 });
