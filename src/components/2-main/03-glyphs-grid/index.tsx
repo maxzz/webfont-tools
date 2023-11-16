8e70afd9465cbcd2418e736b7844ae2d5df5be86
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-15 19:41:14 -0800

    WIP. GlyphCell. reduce nested divs

--- src/components/2-main/03-glyphs-grid/index.tsx
+++ src/components/2-main/03-glyphs-grid/index.tsx
@@ -5,23 +5,18 @@ import { HTMLAttributes } from 'react';
 
 function GlyphCell({ glyph, ...rest }: { glyph: GlyphAttributes; } & HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className="px-1 py-1 border-primary-400 border rounded flex space-x-2" {...rest}>
-
-            <div className="grid">
-                <div className="border-primary-400 border rounded grid place-items-center">
-                    <svg className="fill-foreground bg-background" viewBox="0 0 512 512">
-                        <g transform="scale(1,-1) translate(0,-448) scale(0.9,0.9)">
-                            <path d={glyph.d} />
-                        </g>
-                    </svg>
-                </div>
-
-                <div className="w-8 h-8 text-[.65rem] bg-primary-200 border-primary-400 border rounded flex items-center justify-center">
-                    {glyph.unicode}
-                </div>
-
+        <div className="px-1 py-1 border-primary-400 border rounded grid" {...rest}>
+            <div className="border-primary-400 border rounded grid place-items-center">
+                <svg className="fill-foreground bg-background" viewBox="0 0 512 512">
+                    <g transform="scale(1,-1) translate(0,-448) scale(0.9,0.9)">
+                        <path d={glyph.d} />
+                    </g>
+                </svg>
             </div>
 
+            <div className="w-8 h-8 text-[.65rem] bg-primary-200 border-primary-400 border rounded flex items-center justify-center">
+                {glyph.unicode}
+            </div>
         </div>
     );
 }
