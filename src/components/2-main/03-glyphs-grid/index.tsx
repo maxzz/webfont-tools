6a40ea479ee1a5656af4852fe6d0a3352c70f22a
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-15 19:34:16 -0800

    WIP. GlyphCell. update cells layout and auto-fit

--- src/components/2-main/03-glyphs-grid/index.tsx
+++ src/components/2-main/03-glyphs-grid/index.tsx
@@ -7,19 +7,20 @@ function GlyphCell({ glyph, ...rest }: { glyph: GlyphAttributes; } & HTMLAttribu
     return (
         <div className="px-1 py-1 border-primary-400 border rounded flex space-x-2" {...rest}>
 
-            <div className="flex">
-                <div className="w-8 h-8 bg-primary-200 border-primary-400 border rounded flex items-center justify-center">
-                    {glyph.unicode}
-                </div>
-
+            <div className="grid">
                 <div className="border-primary-400 border rounded grid place-items-center">
                     {/* <svg className="w-16 h-16 bg-blue-300" viewBox="0 0 512 512" transform="scale(1,-1)"> */}
                     <svg className="w-16 h-16 bg-blue-300" viewBox="0 0 512 512">
-                        <g transform="scale(1,-1) translate(0, -448) scale(0.9, 0.9)">
+                        <g transform="scale(1,-1) translate(0,-448) scale(0.9,0.9)">
                             <path d={glyph.d} />
                         </g>
                     </svg>
                 </div>
+
+                <div className="w-8 h-8 text-[.65rem] bg-primary-200 border-primary-400 border rounded flex items-center justify-center">
+                    {glyph.unicode}
+                </div>
+
             </div>
 
         </div>
@@ -29,7 +30,7 @@ function GlyphCell({ glyph, ...rest }: { glyph: GlyphAttributes; } & HTMLAttribu
 export function GlyphsGrid() {
     const snap = useSnapshot(fontData).glyphs;
     return (
-        <div className="text-xs grid grid-cols-[repeat(auto-fit,minmax(16rem,1fr))]">
+        <div className="text-xs grid grid-cols-[repeat(auto-fit,minmax(6rem,1fr))]">
             {snap.map((glyph) => (
                 <GlyphCell glyph={glyph} key={`${glyph.unicode}${glyph['glyph-name'] || ''}`} />
             ))}
