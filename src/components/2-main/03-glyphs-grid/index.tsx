587b7dafb513e10fa63f9957202e12460b7724a7
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-16 13:55:03 -0800

    WIP. GlyphCell. update grid layouting

--- src/components/2-main/03-glyphs-grid/index.tsx
+++ src/components/2-main/03-glyphs-grid/index.tsx
@@ -5,17 +5,22 @@ import { HTMLAttributes } from 'react';
 
 function GlyphCell({ glyph, ...rest }: { glyph: GlyphAttributes; } & HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className="w-12 h-12 bg-red-400/30 border-primary-400 border rounded grid divide-y hover:scale-[2] transition-transform" {...rest}>
+        <div className="group w-min bg-sky-400/20 border-primary-400 border rounded divide-y grid" {...rest}>
 
-            <div className="px-1 py-1 border-primary-400 rounded grid place-items-center">
-                <svg className="1fill-foreground bg-sky-400" viewBox="0 0 512 512">
-                    <g transform="scale(1,-1) translate(0,-448) scale(0.9,0.9)">
+            <div className="px-1 py-1 w-16 h-16 border-primary-400 rounded grid place-items-center group-hover:scale-[2] transition-transform">
+                <svg className="1fill-foreground bg-sky-400" viewBox="0 0 512 512" transform="scale(1,-1)">
+                {/* <svg className="1fill-foreground bg-sky-400/40" viewBox="0 0 1000 1000" transform="scale(1,-1)"> */}
+                    <g
+                        // transform="scale(1,-1) translate(0,-448) scale(0.9,0.9)"
+                    >
                         <path d={glyph.d} />
                     </g>
                 </svg>
             </div>
 
-            <div className="w-8 h-8 text-[.65rem] flex items-center justify-center">
+            <button>copy</button>
+
+            <div className="w-full text-[.65rem] flex items-center justify-center overflow-hidden">
                 {glyph.unicode}
             </div>
         </div>
@@ -25,7 +30,7 @@ function GlyphCell({ glyph, ...rest }: { glyph: GlyphAttributes; } & HTMLAttribu
 export function GlyphsGrid() {
     const snap = useSnapshot(fontData).glyphs;
     return (
-        <div className="text-xs grid grid-cols-[repeat(auto-fit,minmax(3rem,1fr))]">
+        <div className="text-xs grid grid-cols-[repeat(auto-fit,minmax(5rem,1fr))]">
             {snap.map((glyph) => (
                 <GlyphCell glyph={glyph} key={`${glyph.unicode}${glyph['glyph-name'] || ''}`} />
             ))}
