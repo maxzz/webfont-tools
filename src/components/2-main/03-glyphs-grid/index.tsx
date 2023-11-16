7cdb289dde5e5530c44956f51cf0b3cc99f386ff
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-15 19:53:59 -0800

    WIP. GlyphCell. add scale on hover

--- src/components/2-main/03-glyphs-grid/index.tsx
+++ src/components/2-main/03-glyphs-grid/index.tsx
@@ -5,16 +5,17 @@ import { HTMLAttributes } from 'react';
 
 function GlyphCell({ glyph, ...rest }: { glyph: GlyphAttributes; } & HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className="px-1 py-1 border-primary-400 border rounded grid" {...rest}>
-            <div className="border-primary-400 border rounded grid place-items-center">
-                <svg className="fill-foreground bg-background" viewBox="0 0 512 512">
+        <div className="w-12 h-12 bg-red-400/30 border-primary-400 border rounded grid divide-y hover:scale-[2] transition-transform" {...rest}>
+
+            <div className="px-1 py-1 border-primary-400 rounded grid place-items-center">
+                <svg className="1fill-foreground bg-sky-400" viewBox="0 0 512 512">
                     <g transform="scale(1,-1) translate(0,-448) scale(0.9,0.9)">
                         <path d={glyph.d} />
                     </g>
                 </svg>
             </div>
 
-            <div className="w-8 h-8 text-[.65rem] bg-primary-200 border-primary-400 border rounded flex items-center justify-center">
+            <div className="w-8 h-8 text-[.65rem] flex items-center justify-center">
                 {glyph.unicode}
             </div>
         </div>
