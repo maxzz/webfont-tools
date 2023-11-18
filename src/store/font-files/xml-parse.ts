6f7a470b2a42f0750bdf23d7dee50344afd2a95f
Max Zakharzhevskiy <maxzz@msn.com>
2023-11-17 18:29:25 -0800

    WIP. store. bug. join truncates array

--- src/store/font-files/xml-parse.ts
+++ src/store/font-files/xml-parse.ts
@@ -16,6 +16,32 @@ export function xml2Js(cnt: string) {
 
 // XML beautifier similar to https://gist.github.com/sente/1083506
 
+export function formatXml2(xml: string, tab = '  ') { // tab = optional indent value, default is tab (\t)
+    tab = tab || '\t';
+
+    const formatted: string[] = [];
+    let indent = '';
+
+    xml.split(/>\s*</).forEach(
+        (node) => {
+            if (node.match(/^\/\w/)) {
+                indent = indent.substring(tab.length); // decrease indent by one 'tab'
+            }
+
+            // formatted += `${indent}<${node}>\r\n`; //TODO: array.join('\r\n') will be more efficient here
+            formatted.push(`${indent}<${node}>`); //TODO: array.join('\r\n') will be more efficient here
+
+            if (node.match(/^<?\w[^>]*[^\/]$/)) {
+                indent += tab;              // increase indent
+            }
+        }
+    );
+
+    console.log('formatted', formatted.join('\r\n').substring(1, formatted.length - 3), `\n\ndone`);
+
+    return formatted.join('\r\n').substring(1, formatted.length - 3);
+}
+
 export function formatXml(xml: string, tab = '  ') { // tab = optional indent value, default is tab (\t)
     tab = tab || '\t';
 
@@ -35,6 +61,8 @@ export function formatXml(xml: string, tab = '  ') { // tab = optional indent va
             }
         }
     );
+
+    console.log('formatted', formatted.substring(1, formatted.length - 3), `\n\ndone`);
 
     return formatted.substring(1, formatted.length - 3);
 }
